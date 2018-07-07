6efdf6a4bae5238e3e417f096a8d807132dd2518
Mariusz Materek <mariusz.materek+bitbucket.org@gmail.com>
2018-07-08 00:22:46 +0100

    Select row implemented when row selected and disappear after. Tick mark added for selection and removed if selected again

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -34,5 +34,25 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    //MARK: Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        } else {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
 }
 
